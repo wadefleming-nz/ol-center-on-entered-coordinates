b2ef64a1a9e243c1faf814038f55b07ca8e32929
wflemingnz <wadefleming2000@hotmail.com>
2020-06-07 23:22:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ol-center-on-entered-coordinates
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ol-center-on-entered-coordinates)
---
+++ index.js
@@ -0,0 +1,50 @@
+import './style.css';
+import 'ol/ol.css'
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import { Tile as TileLayer, Vector as VectorLayer, } from 'ol/layer.js';
+import { OSM, Vector as VectorSource } from 'ol/source.js';
+import Feature from 'ol/Feature.js';
+import { LineString, Point } from 'ol/geom.js';
+import { fromLonLat } from 'ol/proj.js';
+import { Stroke, Style, Icon } from 'ol/style.js';
+
+var london = fromLonLat([-0.12755, 51.507222]);
+
+var map = new Map("map");
+var raster = new TileLayer({
+  source: new OSM()
+});
+
+var view = new View({
+  center: london,
+  zoom: 4
+});
+
+var map = new Map({
+  layers: [raster],
+  target: 'map',
+  view: view,
+  loadTilesWhileAnimating: true,
+});
+
+const coordsElement = document.getElementById('coords');
+coordsElement.addEventListener('change', onCoordsEntered);
+ 
+function onCoordsEntered(e){
+  var latLong = e.target.value.split(",");  // parse lat,long user input
+  if(latLong.length == 2){
+    var coords = fromLonLat([latLong[1], latLong[0]]); // flip lat/long for input to fromLonLat
+      console.log(coords);
+
+   // animate to the entered location
+   view.animate({
+      center: coords,
+      duration: 500,
+   });
+
+   // use this instead if you don't want animation
+   // view.setCenter(coords);
+  }
+}
+
